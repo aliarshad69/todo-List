0bdff35752df24d64b52c60effcdd605fd4ad10c
unknown <aliarshad9566694@gmail.com>
2022-12-02 15:39:11 +0800

    templet,css added

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+
 var itemsArr = ['Buy Food','cook food','eat food']
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(express.static('public'));
 app.get('/',function(req,res){
 
     var today = new Date()
@@ -18,27 +19,9 @@ app.get('/',function(req,res){
     }
     var day = today.toLocaleDateString('en-US',options)
     // *****************************************************************************************
-    var today = new Date();
-    // var currentDay =  today.getDay()
-    // var d = new Date();
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    var d = new Date();
-    var currentDay = d.getDay()
-    var dayName = days[d.getDay()];
-    var dayTimeH = d.getHours()
-    var dayTimeM = d.getMinutes()
-    var dayTimeS = d.getSeconds()
-    // console.log(dayName+" "+dayTimeH+'hrs:'+dayTimeM+"min:"+dayTimeS+'sec')
-    var notice = ''
-    if (currentDay === 6 || currentDay === 0){
-        notice = dayName+" "+dayTimeH+'hrs:'+dayTimeM+"min:"+dayTimeS+'sec' + '***WEEKEND***'
-    }
-    else{
-        notice = dayName+" "+dayTimeH+'hrs:'+dayTimeM+"min:"+dayTimeS+'sec' + '!!!WEEKDAY!!!'
-    }
-  //  ************************************************************************************************
+   
     res.render('lists', {
-        KindOfDay: day,NewAddedItem: itemsArr,KindOfDayy: notice
+        KindOfDay: day,NewAddedItem: itemsArr
     })
     
     
