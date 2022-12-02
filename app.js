33876dc3d92cd5ea5dd39739134b2c0622ccb316
unknown <aliarshad9566694@gmail.com>
2022-12-02 19:12:43 +0800

    main and work templet-seprated by button name and value-

--- app.js
+++ app.js
@@ -4,33 +4,74 @@ const bodyParser = require('body-parser')
 const app = express()
 
 
-var itemsArr = ['Buy Food','cook food','eat food']
+let itemsArr = ['Buy Food','cook food','eat food']
+let workArr = []
+
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
+// ****************************************************************************
 app.get('/',function(req,res){
-
-    var today = new Date()
-    var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-    }
-    var day = today.toLocaleDateString('en-US',options)
-    // *****************************************************************************************
-   
-    res.render('lists', {
-        KindOfDay: day,NewAddedItem: itemsArr
+  
+  let today = new Date()
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  let day = today.toLocaleDateString('en-US',options)
+  // *****************************************************************************************
+  
+  res.render('lists', {
+      KindOfDay: day, Title: "Main", NewAddedItem: itemsArr
+  
     })
-    
     
 })
 app.post('/', function(req,res){
-  var item = req.body.newItem
-  itemsArr.push(item)
-  res.redirect('/')
+  
+  if (req.body.button === "Main"){
+    let item = req.body.newItem
+    itemsArr.push(item)
+    res.redirect('/')
+    // console.log(req.body)
+  }
+  else{
+      let workitem = req.body.newItem
+      workArr.push(workitem)
+      res.redirect('/work')
+      // console.log(req.body)
+      
+    // })
+  }
+  
 })
+// *********************************************************************************
+
+// ************************************************************************************
+app.get('/work',function(req,res){
+  let today = new Date()
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  let day = today.toLocaleDateString('en-US',options)
+  res.render('lists', {
+    KindOfDay: day, Title: "Work", NewAddedItem: workArr
+})
+  
+}) 
+app.post('/work', function(req,res){
+  let workitem = req.body.newItem
+  workArr.push(workitem)
+  res.redirect('/work')
+  // console.log(req.body)
+  
+})
+// *******************************************************************************************
+
 
 app.listen(3000,function(){
     console.log('server started on port 3000')
